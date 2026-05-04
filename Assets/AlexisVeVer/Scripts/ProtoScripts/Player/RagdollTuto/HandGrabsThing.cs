7653b3f6739e50391e20536337d1d7825f147a39
Alexis Verne-rey <a.verne@nouvellesimages.xyz>
2026-05-04 16:58:57 +0200

    Grab major advancements
    Sur la scene  d'alexis

--- Assets/AlexisVeVer/Scripts/ProtoScripts/Player/RagdollTuto/HandGrabsThing.cs
+++ Assets/AlexisVeVer/Scripts/ProtoScripts/Player/RagdollTuto/HandGrabsThing.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,13 +6,42 @@ namespace AlexisVeVer.Scripts.ProtoScripts.Player.RagdollTuto
 {
     public class HandGrabsThing : MonoBehaviour
     {
+        [SerializeField] private GameObject _previousParent;
+        private GameObject _collidedPlayer;
+        
         void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
-                other.transform.SetParent(transform);
+                if (other.gameObject.transform.parent != null)
+                {
+                    _previousParent = other.gameObject.transform.parent.gameObject;
+                }
+                other.gameObject.transform.SetParent(transform);
             }
         }
-        
+
+        void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                other.gameObject.transform.SetParent(transform);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (_previousParent != null)
+                {
+                    other.gameObject.transform.parent.SetParent(_previousParent.transform);
+                }
+                else
+                {
+                    other.gameObject.transform.SetParent(null);
+                }
+            }
+        }
     }
 }
