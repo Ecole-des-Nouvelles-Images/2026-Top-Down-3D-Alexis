8d1b2c1a6bc46565f817a654127b46e1a0c003ff
Baptiste Gelly <gellybaptiste@gmail.com>
2026-05-18 14:46:10 +0200

    rename

--- Assets/Baptiste/script/Transition.cs
+++ Assets/Baptiste/script/Transition.cs
@@ -1,43 +1,44 @@
-using System;
-using UnityEngine;
-using UnityEngine.EventSystems;
 using DG.Tweening;
+using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-public class transition2 : MonoBehaviour
+
+namespace Baptiste.script
 {
-    [SerializeField] private float speed = 4f;
-    [Space(5)]
-    [SerializeField] private Vector2 _startAnchorMin= new Vector2(0,0);
-    [SerializeField] private Vector2 _startAnchorMax= new Vector2(1,0);
-    [Space(5)]
-    [SerializeField] private Vector2 _endAnchorMin=new Vector2(0,3);
-    [SerializeField] private Vector2 _endAnchorMax=new Vector2(1,3);
-    [Space (5)] 
-    [SerializeField] private bool _isTransitioning= false;
-    [SerializeField] private float _timer = 0;
-    [SerializeField] private float _transitionDuration = 0.5f;
-    [SerializeField] private string _nameScene;
-    
-    void Start()
+    public class Transition : MonoBehaviour
     {
-        RectTransform rect = transform.GetComponent<RectTransform>();
-        rect.anchorMin = _startAnchorMin;
-        rect.anchorMax = _startAnchorMax;
-        rect.DOAnchorMin(_endAnchorMin, speed);
-        rect.DOAnchorMax(_endAnchorMax, speed);
-    }
+        [SerializeField] private float speed = 4f;
+        [Space(5)]
+        [SerializeField] private Vector2 _startAnchorMin= new Vector2(0,0);
+        [SerializeField] private Vector2 _startAnchorMax= new Vector2(1,0);
+        [Space(5)]
+        [SerializeField] private Vector2 _endAnchorMin=new Vector2(0,3);
+        [SerializeField] private Vector2 _endAnchorMax=new Vector2(1,3);
+        [Space (5)] 
+        [SerializeField] private bool _isTransitioning= false;
+        [SerializeField] private float _timer = 0;
+        [SerializeField] private float _transitionDuration = 0.5f;
+        [SerializeField] private string _nameScene;
     
-    private void Update()
-    {
-        if (_isTransitioning = true)
+        void Start()
         {
-            _timer += Time.deltaTime;
-            if (_timer >= _transitionDuration)
-            {
-                SceneManager.LoadScene(_nameScene);
-            }
+            RectTransform rect = transform.GetComponent<RectTransform>();
+            rect.anchorMin = _startAnchorMin;
+            rect.anchorMax = _startAnchorMax;
+            rect.DOAnchorMin(_endAnchorMin, speed);
+            rect.DOAnchorMax(_endAnchorMax, speed);
         }
+    
+        private void Update()
+        {
+            if (_isTransitioning = true)
+            {
+                _timer += Time.deltaTime;
+                if (_timer >= _transitionDuration)
+                {
+                    SceneManager.LoadScene(_nameScene);
+                }
+            }
        
+        }
     }
 }
